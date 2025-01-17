5c05cef77e5ab462ccdd6cfdb6c4c2399a54cad0
SoundaryaRajasekaren <soundaryarajase12@gmail.com>
2025-01-17 12:08:08 +0530

    feat: added auth page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,8 +1,19 @@
 const Auth = () => {
   return (
-    <section>
-      <div>Auth</div>
-    </section>
+    <div className="auth-sign-in">
+      <div className="sign-in">
+        <h1>Welcome Back👋</h1>
+        <p>This is John&apos;s page</p>
+
+        <label className="mobile-num">Mobile Number:</label>
+        <input type="tel" placeholder="123-456-7890" className="mobile-input" />
+        <a href="#">Forgot Password?</a>
+        <button>Sign In</button>
+      </div>
+      <div className="sign-in-image">
+        <img src=".\imagesauth\flower images.jpg"></img>
+      </div>
+    </div>
   )
 }
 
