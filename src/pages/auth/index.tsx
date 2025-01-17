6521eae1cfaf6c7bf7356e8fe359be997e50d8a2
SoundaryaRajasekaren <soundaryarajase12@gmail.com>
2025-01-17 16:12:09 +0530

    feat: added input OTP to the auth

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,8 +1,12 @@
+import { useState } from 'react'
 import { Button } from 'src/components/ui/button'
 import { Input } from 'src/components/ui/input'
+import { InputOTP, InputOTPGroup, InputOTPSeparator, InputOTPSlot } from 'src/components/ui/input-otp'
 import { Label } from 'src/components/ui/label'
 
 const Auth = () => {
+  const [isMobileTrue, setisMobileTrue] = useState(false)
+
   return (
     <div className="mx-auto flex h-screen max-w-md flex-col px-6 py-4 lg:mx-0 lg:max-w-none lg:flex-row lg:px-6 lg:py-4">
       <div className="order-2 px-0 py-0 lg:order-1 lg:w-1/2 lg:px-32 lg:py-16">
@@ -12,19 +16,38 @@ const Auth = () => {
             Today is a new day. It&apos;s your day. You shape it. Sign in to start managing your projects.
           </p>
         </div>
-        <div>
-          <Label>Phone Number</Label>
-          <div className="flex items-center gap-2">
-            <Button className="flex h-full w-[125px] font-semibold" variant="outline">
-              🏁 (+91)
-            </Button>
-            <Input placeholder="8790988098" />
+        {!isMobileTrue ? (
+          <div>
+            <Label>Phone Number</Label>
+            <div className="flex items-center gap-2">
+              <Button className="flex h-full w-[125px] font-semibold" variant="outline">
+                🏁 (+91)
+              </Button>
+              <Input placeholder="8790988098" />
+            </div>
+            <a className="flex justify-end pt-2 text-sm hover:text-red-400" href="#">
+              Forgot Password?
+            </a>
           </div>
-        </div>
-        <a className="flex justify-end pt-2 text-sm hover:text-red-400" href="#">
-          Forgot Password?
-        </a>
-        <Button className="my-4 w-full" variant="primary">
+        ) : (
+          // Here you need to add the OTP input field.
+          <div className="flex w-full justify-center">
+            <InputOTP maxLength={6}>
+              <InputOTPGroup>
+                <InputOTPSlot index={0} />
+                <InputOTPSlot index={1} />
+                <InputOTPSlot index={2} />
+              </InputOTPGroup>
+              <InputOTPSeparator />
+              <InputOTPGroup>
+                <InputOTPSlot index={3} />
+                <InputOTPSlot index={4} />
+                <InputOTPSlot index={5} />
+              </InputOTPGroup>
+            </InputOTP>
+          </div>
+        )}
+        <Button onClick={() => setisMobileTrue(true)} className="my-4 w-full" variant="primary">
           Sign In
         </Button>
         <p className="flex justify-center pt-8 text-sm">
