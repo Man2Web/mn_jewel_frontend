86d390e900f9474398705d4da5aaf9b48e40e735
SoundaryaRajasekaren <soundaryarajase12@gmail.com>
2025-01-17 14:29:44 +0530

    feat: updated auth page

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,17 +1,41 @@
+import { Button } from 'src/components/ui/button'
+import { Input } from 'src/components/ui/input'
+import { Label } from 'src/components/ui/label'
+
 const Auth = () => {
   return (
-    <div className="auth-sign-in">
-      <div className="sign-in">
-        <h1>Welcome Back👋</h1>
-        <p>This is John&apos;s page</p>
-
-        <label className="mobile-num">Mobile Number:</label>
-        <input type="tel" placeholder="123-456-7890" className="mobile-input" />
-        <a href="#">Forgot Password?</a>
-        <button>Sign In</button>
+    <div className="mx-auto flex h-screen max-w-md flex-col px-6 py-4 lg:mx-0 lg:max-w-none lg:flex-row lg:px-6 lg:py-4">
+      <div className="order-2 px-0 py-0 lg:order-1 lg:w-1/2 lg:px-32 lg:py-16">
+        <div className="flex flex-col gap-2 py-4">
+          <h1 className="text-2xl">Welcome Back👋</h1>
+          <p className="text-sm">
+            Today is a new day. It&apos;s your day. You shape it. Sign in to start managing your projects.
+          </p>
+        </div>
+        <div>
+          <Label>Phone Number</Label>
+          <div className="flex items-center gap-2">
+            <Button className="flex h-full w-[125px] font-semibold" variant="outline">
+              🏁 (+91)
+            </Button>
+            <Input placeholder="8790988098" />
+          </div>
+        </div>
+        <a className="flex justify-end pt-2 text-sm hover:text-red-400" href="#">
+          Forgot Password?
+        </a>
+        <Button className="my-4 w-full" variant="primary">
+          Sign In
+        </Button>
+        <p className="flex justify-center pt-8 text-sm">
+          Dont have an Account?{' '}
+          <a className="pl-1 text-red-400" href="#">
+            Sign Up
+          </a>
+        </p>
       </div>
-      <div className="sign-in-image">
-        <img src=".\imagesauth\flower images.jpg"></img>
+      <div className="order-1 h-40 w-full lg:order-2 lg:h-full lg:w-1/2">
+        <img className="h-full w-full rounded-lg" src="https://images4.alphacoders.com/133/1339103.png" />
       </div>
     </div>
   )
